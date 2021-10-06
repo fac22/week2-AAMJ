4f2b9c9eb3a3d2dfc69cfd008966d808c12c0658
aaadriana <adriana@stanciu.uk>
2021-10-06 12:25:47 +0100

    Install ESLint and Prettier
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: MJOW1999 <72146072+MJOW1999@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (request, response) => {
-  response.send('hello world');
+server.get("/", (request, response) => {
+  response.send("hello world");
 });
 
 const PORT = process.env.PORT || 3000;
