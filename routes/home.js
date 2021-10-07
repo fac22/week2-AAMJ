fc4a48bbd8ea018d37b36a1fbbae1f14c211efa3
jihye <jiji.pak41@gmail.com>
2021-10-07 10:59:49 +0100

    cupdate form
    Co-authored-by: MJOW1999 <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: aaadriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>

--- routes/home.js
+++ routes/home.js
@@ -5,23 +5,44 @@ function get(request, response) {
   const html = /*html*/ `
   
   <form method="POST" class="stack-md center width-sm justify-space-between" id="posting-form">
-            <!-- // Author -->
-            <label for="new-author">User name</label>
-            <input type="text" id="new-author" name="newAuthor" placeholder="Your name" required>
+            <!-- // Name -->
+            <label for="username">User name</label>
+            <input type="text" id="username" name="username" placeholder="Your name" required>
   
-            <!-- Title -->
-            <label for="new-title">Title</label>
-            <input type="text" id="new-title" name="newTitle" placeholder="Write a unique post title" required>
+            <!-- email -->
+            <label for="user_email">Email</label>
+            <input type="email" id="user_email" name="user_email" placeholder="Enter your email" required>
   
             <!-- New Message -->
-            <label for="new-txt">Message</label>
-            <textarea
-              id="new-xt"
-              name="newTxt" maxlength="100" placeholder="What's on your mind? max length 100 characters"
-            required></textarea>
-            <button id="new-send" name="new-send" aria-label="Send post">Send</button>
+            <button id="submit-details" name="submit-details" aria-label="Submit your details">Send</button>
   
           </form>
+
+          <form method="POST" class="stack-md center width-sm justify-space-between" id="posting-form">
+          <!-- // Name -->
+          <label for="book_name">Book name</label>
+          <input type="text" id="book_name" name="book_name" placeholder="Book name" required>
+
+          <!-- Author -->
+          <label for="author">Author</label>
+          <input type="text" id="author" name="author" placeholder="Book author" required>
+
+          <!-- Description -->
+          <label for="desc">Book description</label>
+          <textarea
+              id="desc"
+              name="desc" maxlength="100" placeholder="Book description in 100 characters or less"
+            required></textarea>
+
+
+          <!-- Genre -->
+          <label for="genre">Genre</label>
+          <input type="text" id="genre" name="genre" placeholder="Book genre" required>
+
+
+          <button id="submit-details" name="submit-details" aria-label="Submit your details">Send</button>
+
+        </form>
      `;
 
   response.send(html);
